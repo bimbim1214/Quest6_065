3a973fb597427fc143468f8a3631de939f601acf
bimbim1214 <bimoadityapangestu@gmail.com>
2024-11-21 16:59:41 +0700

    membuat objek matakuliah untuk isian dropdown nanti nya

---
+++ app/src/main/java/com/example/pertemuan8/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan8.data
+
+object MataKuliah {
+    val options= listOf(
+        "Agama Islam dan Kemuhamadiayahan",
+        "Pengembangan Aplikasi WEB",
+        "Pancasila",
+        "Dasar Teknologi Informasi",
+        "Fisika dan Elektronika",
+        "Logika teknik pemrograman",
+    )
+}
