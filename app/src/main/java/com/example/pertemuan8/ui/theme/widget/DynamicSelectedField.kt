8434118ce42ff18514c46bb6b5107ae5d21002d5
bimbim1214 <bimoadityapangestu@gmail.com>
2024-11-22 10:57:24 +0700

    menambahkan logika iterasi untuk menu dropdown

--- app/src/main/java/com/example/pertemuan8/ui/theme/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/pertemuan8/ui/theme/widget/DynamicSelectedField.kt
@@ -50,6 +50,18 @@ fun DynamicSelectTextField(
                 .menuAnchor()
                 .fillMaxWidth()
         )
-
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {expanded = false}) {
+            options.forEach{ option : String ->
+                DropdownMenuItem(
+                    text = { Text(text = option)},
+                    onClick = {
+                        expanded = false
+                        onValueChangeEvent(option)
+                    }
+                )
+            }
+        }
     }
 }
