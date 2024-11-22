899af8f51c4fa8a7032b67045b69ecdd3aef394e
bimbim1214 <bimoadityapangestu@gmail.com>
2024-11-22 10:35:34 +0700

    membuat class RencanaStudiViewModel dan membuat fungsi update untuk mengupdate mk pilihan dan kelas

---
+++ app/src/main/java/com/example/pertemuan8/ui/theme/view/viewmodel/RencanaStudiViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.pertemuan8.ui.theme.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudiViewModel: ViewModel() {
+    private val _krsState =  MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK ->stateMK.copy(mataKuliah = mkPilihan) }
+    }
+
+    fun settKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas ->stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status ->status.copy(
+            mataKuliah = ls[0],
+            kelas = ls[1]
+        ) }
+    }
+}
