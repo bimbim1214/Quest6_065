6705963d1ba1130588c9a8022fff808e42ee4d34
bimbim1214 <bimoadityapangestu@gmail.com>
2024-11-22 12:35:21 +0700

    membuat jarak antara conten dengan spacer lalu buat column yg di dalamnya terdapat 2 text dan box yang berisi icon

--- app/src/main/java/com/example/pertemuan8/ui/theme/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/theme/view/screen/RencanaStudiView.kt
@@ -75,8 +75,32 @@ fun RencanaStudiView(
                     .clip(shape = CircleShape)
                     .size(50.dp)
             )
-
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
+            Box{
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
         }
+
 
     }
 
